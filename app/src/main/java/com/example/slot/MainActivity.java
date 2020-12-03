b073950231be64f6f630a53d503cb9f77a5cd215
zzrihen <zohar8395@gmail.com>
2020-12-03 13:15:06 +0200

    2nd commit

--- app/src/main/java/com/example/slot/MainActivity.java
+++ app/src/main/java/com/example/slot/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.slot;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button= findViewById(R.id.button_register);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                openSignUpActivity();
+            }
+            });
+        }
+
+    public void openSignUpActivity(){
+        Intent intent = new Intent(this, SignUpActivity.class);
+        startActivity(intent);
     }
 }
