834492ab94c1ae5872c799c1ceb131d9ae57b52b
zzrihen <zohar8395@gmail.com>
2020-12-07 02:10:36 +0200

    Student main page without DB

---
+++ app/src/main/java/com/example/slot/setAppointmentStudentActivity.java
@@ -0,0 +1,14 @@
+package com.example.slot;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class setAppointmentStudentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_set_appointment_student);
+    }
+}
