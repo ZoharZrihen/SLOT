401e4fd0fa317769e7a98ef2b4ce143c9600bc09
zzrihen <zohar8395@gmail.com>
2020-12-11 17:09:42 +0200

    professor show works, sorted!

--- app/src/main/java/com/example/slot/student/setAppointmentStudentActivity.java
+++ app/src/main/java/com/example/slot/student/setAppointmentStudentActivity.java
@@ -27,6 +27,7 @@ import com.google.firebase.database.ValueEventListener;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
@@ -142,7 +143,7 @@ public class setAppointmentStudentActivity extends AppCompatActivity  {
         HashMap<String, Boolean> _slots = (HashMap<String, Boolean>) course_info.get("slots");
         ArrayList<String> available_slots = new ArrayList<>();
         //Checking for available slots
-        available_slots.add("בחר SLOT");
+//        available_slots.add("בחר SLOT");
         for( String slot : _slots.keySet()){
             try {
                 if(_slots.get(slot) == true){
@@ -159,7 +160,8 @@ public class setAppointmentStudentActivity extends AppCompatActivity  {
             //Too full, display a message to user.
             Toast.makeText(this, "אין מקום !", Toast.LENGTH_LONG);
         }
-
+        Collections.sort(available_slots);
+        available_slots.add(0, "בחר SLOT");
         ArrayAdapter<String> adapter =
                 new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item,available_slots){
                     @Override
@@ -209,6 +211,9 @@ public class setAppointmentStudentActivity extends AppCompatActivity  {
                         public void onClick(View v) {
                             String uid=FirebaseAuth.getInstance().getUid();
                             UpdateUser(chosen_course,chosen_time,uid);
+                            Toast.makeText(setAppointmentStudentActivity.this, "שעת קבלה נקבעה בהצלחה!",Toast.LENGTH_LONG).show();
+                            startActivity(new Intent(setAppointmentStudentActivity.this,StudentMainActivity.class));
+                            finish();
 
                         }
                     });
